574d6b2bccad8306009c59190e20a157b2923c1a
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2023-01-25 21:45:13 -0500

    🥕 Add Screens Componenets and start work on StartGameScreen.js

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello World!!!!</Text>
       <StatusBar style="auto" />
+      <StartGameScreen />
     </View>
   );
 }
---
+++ screens/StartGameScreen.js
@@ -0,0 +1,12 @@
+import { Text, TextInput, View, Button,  } from "react-native"
+
+function StartGameScreen() {
+  return (
+    <View>
+      <TextInput />
+      <Text>StartGameScreen</Text>
+    </View>
+  )
+}
+
+export default StartGameScreen
