289f4cb532354b192fbfe704733c312ce69c218c
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2023-01-26 20:47:44 -0500

    🥕 Add ImageBackground Component used as Background

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, ImageBackground } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { LinearGradient } from 'expo-linear-gradient';
 
@@ -11,7 +11,14 @@ export default function App() {
       colors={['#4e0329', '#ddb52f']}
       >
       <StatusBar style='light'/>
-      <StartGameScreen />
+      <ImageBackground 
+        source={require('./assets/images/background.png')} 
+        resizeMode="cover"
+        style={styles.rootScreen}
+        imageStyle={styles.imageBackground}
+        >
+        <StartGameScreen />
+      </ImageBackground>
     </LinearGradient>
   );
 };
@@ -19,5 +26,8 @@ export default function App() {
 const styles = StyleSheet.create({
   rootScreen:{
     flex: 1,
+  },
+  imageBackground:{
+    opacity: 0.15,
   }
 });
