5a607e9629763689f3b1c5716b52c540917a12ae
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2023-02-19 18:59:01 -0500

    🥕 Add styles and conbetn on GameOverScreen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,9 +1,38 @@
-import {Text, View, StyleSheet, Alert} from 'react-native'
+import {View, Text, StyleSheet, Image} from 'react-native'
+
+import Title from '../components/ui/Title';
+import Colors from '../constants/colors';
 
 export default function GameOverScreen() {
   return (
-    <View>
-      <Text>GameOverScreen</Text>
+    <View style={styles.rootContainer}>
+      <Title>Game Over!</Title>
+      <View style={styles.imageContainer}>
+        <Image style={styles.image} source={require('../assets/images/success.png')} />
+      </View>
+      <Text>Your phone needed X rounds to guess the number Y</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  rootContainer:{
+    flex: 1,
+    padding: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageContainer:{
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: Colors.primary800,
+    overflow: 'hidden',
+    margin: 36,
+  },
+  image:{
+    width: '100%',
+    height: '100%',
+  },
+});
