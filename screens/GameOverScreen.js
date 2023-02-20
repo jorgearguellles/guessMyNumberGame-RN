a7f2c443a8a8e8454e71d2e2314add0899036584
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2023-02-19 19:08:39 -0500

    🥕 Add GameOver Stylea and info distribution

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -2,6 +2,7 @@ import {View, Text, StyleSheet, Image} from 'react-native'
 
 import Title from '../components/ui/Title';
 import Colors from '../constants/colors';
+import PrimaryButton from '../components/ui/PrimaryButton';
 
 export default function GameOverScreen() {
   return (
@@ -10,7 +11,10 @@ export default function GameOverScreen() {
       <View style={styles.imageContainer}>
         <Image style={styles.image} source={require('../assets/images/success.png')} />
       </View>
-      <Text>Your phone needed X rounds to guess the number Y</Text>
+      <Text style={styles.summaryText}>
+        Your phone needed <Text style={styles.highlight}>X</Text> rounds to guess the number <Text style={styles.highlight}>Y</Text>
+      </Text>
+      <PrimaryButton>Start New game</PrimaryButton>
     </View>
   )
 }
@@ -35,4 +39,14 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
   },
+  summaryText:{
+    fontFamily: 'open-sans',
+    fontSize: 24,
+    textAlign: 'center',
+    marginBottom: 24,
+  },
+  highlight:{
+    fontFamily: 'open-sans-bold',
+    color: Colors.primary500,
+  }
 });
