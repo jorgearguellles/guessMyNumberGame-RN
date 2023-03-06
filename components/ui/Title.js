ad200e479ea3a6b382b3ea5dd9e9f34cff5be4a9
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2023-03-05 20:14:19 -0500

    🥕 Add Platform API to handle custom ios and android styles

--- components/ui/Title.js
+++ components/ui/Title.js
@@ -1,4 +1,4 @@
-import {Text, StyleSheet} from 'react-native';
+import {Text, StyleSheet, Platform} from 'react-native';
 
 export default function Title({children}) {
 
@@ -13,10 +13,10 @@ const styles = StyleSheet.create({
   title: {
     fontFamily: 'open-sans-bold',
     fontSize: 24,
-    // fontWeight: 'bold',
     color: 'white',
     textAlign: 'center',
-    borderWidth: 2,
+    // borderWidth: Platform.OS === 'android' ? 2 : 0,
+    borderWidth: Platform.select({ ios: 0, android: 2 }), // Different way to handle styles by Platform
     borderColor: 'white',
     padding: 12,
     maxWidth: '80%',
